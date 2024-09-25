88bfd6905e60713db48cbba2d2a55fc0d8d9a4f3
mirrus <sanserif8pt@gmail.com>
2024-09-25 21:01:58 +0200

    init for tutorial

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from "./components/login/login.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, CommonModule, RouterModule, LoginComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent
+{
+  title = 'tailpdf';
+}
